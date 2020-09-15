5ae75872afc8d218e722b3e66256b26c18e0c482
nir nicole <niko426@gmail.com>
2020-09-15 04:13:53 +0300

    Update LifeSitesGrid.java

--- Cellular automaton/LifeSitesGrid.java
+++ Cellular automaton/LifeSitesGrid.java
@@ -11,12 +11,13 @@ public class LifeSitesGrid extends JFrame {
 	private JPanel[][] gridPanels;	//i gave the class panels matrix character so we could later access each panel and change his color.
 	
 	//i chose to implement only a costume(lines,columns) grid with no default option.
-	public LifeSitesGrid(int lines, int columns) {
+	public LifeSitesGrid(int lines, int columns, int size) {
 		super("Life Game");
 		this.setLayout(new GridLayout(lines,columns,1,1));
 		this.gridPanels = new JPanel[lines][columns];
-		this.setSize(800,800);
+		this.setSize(size,size);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 		
 		//adding the panels to the jframe grid layout.
 		for(int line=0; line<lines; line++)
@@ -26,7 +27,7 @@ public class LifeSitesGrid extends JFrame {
 				this.add(this.gridPanels[line][column]);
 				}
 	}
-	
+
 	public void paintGrid(LifeSitesMatrix lifeMatrix, Color c1, Color c2) {
 		
 		for(int line=0; line<this.gridPanels.length; line++)
@@ -37,6 +38,7 @@ public class LifeSitesGrid extends JFrame {
 					this.gridPanels[line][column].setBackground(c2);
 		this.repaint();
 	}
+	
 }
 
 
